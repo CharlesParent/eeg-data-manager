172a6665f256d82cfad4f473c48a9198e2757d3e
CharlesParent <cha.parent@gmail.com>
2019-08-20 23:48:34 +0200

    Add function to readData

--- data_generators.c
+++ data_generators.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdio.h>
 
+#include "data_generators.h"
 #include "time_utils.h"
 
 #define EEG_DATA_PERIOD 5000 // Period in us
@@ -34,4 +35,16 @@ void* threadACCGenerator(void* args)
 		printf("ACC Data available ! at %lu\n", next.tv_nsec);
 	}
 	return NULL;
+}
+
+void readEEGData(struct eegData *eeg_sample) {
+	eeg_sample->eeg1 = 0;
+	eeg_sample->eeg2 = 0;
+	eeg_sample->eeg3 = 0;
+}
+
+void readACCData(struct accData *acc_sample) {
+	acc_sample->accX = 0;
+	acc_sample->accY = 0;
+	acc_sample->accZ = 0;
 }
--- data_generators.h
+++ data_generators.h
@@ -1,6 +1,20 @@
 #ifndef DATA_GENERATORS
 #define DATA_GENERATORS
 
+#include <stdint.h>
+
+struct eegData {
+	uint32_t eeg1;
+	uint32_t eeg2;
+	uint32_t eeg3;
+};
+
+struct accData {
+	uint16_t accX;
+	uint16_t accY;
+	uint16_t accZ;
+};
+
 void* threadEEGGenerator(void* args);
 void* threadACCGenerator(void* args);
 
